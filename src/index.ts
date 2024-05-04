91a2e4595d536bb59384a5f6fe9ada214e316e90
Eithan <eithantuit10gmail.com>
2024-05-05 02:47:17 +1000

    fix: update timeout

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { createExporterServer } from "./exporter.js";
 import { createIotServer } from "./iot.js";
 
 // Duration sockets will be kept alive before timing out.
-const iotServerTimeout = 30000;
+const iotServerTimeout = 1000 * 60 * 2;
 
 const main = async () => {
   createIotServer({
